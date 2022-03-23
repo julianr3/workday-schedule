07f00da3a50727e53a30f53014b2ee2cc779d20b
Julian Richardson <jrichar3@yahoo.com>
2022-03-23 17:56:20 -0400

    cleaned up script code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,2 @@
-// $(document).ready(function () {
-//     let thisMoment = moment().format("dddd, MMMM do YYYY");
-//     let todaysDate = document.getElementById(currentDay);
-//     todaysDate.innerHTML = thisMoment();
-// })
-
-// var thisMoment = moment();
-// $("currentDay").text(thisMoment.format("dddd, MMMM do YYYY"));
-
-
 const currentDate = moment().format('dddd, MMMM Do YYYY');
 document.getElementById("currentDay").textContent = currentDate;

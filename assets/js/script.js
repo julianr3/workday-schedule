2196b8052e7bf6512768ab42cfac14f26a6f5920
Julian Richardson <jrichar3@yahoo.com>
2022-03-24 15:32:06 -0400

    added local storage to save input text for time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,45 @@
 // WHEN I refresh the page
 // THEN the saved events persist
 
+// let currentHour = moment().format("HH");
+// console.log(currentHour)
+
+
+var nineHourBlock = $("#hour9");
+var tenHourBlock = $("#hour10");
+var elevenHourBlock = $("#hour11");
+var twelveHourBlock = $("#hour12");
+var oneHourBlock = $("#hour13");
+var twoHourBlock = $("#hour14");
+var threeHourBlock = $("#hour15");
+var fourHourBlock = $("#hour16");
+var fiveHourBlock = $("#hour17");
 
 
 const currentDate = moment().format('dddd, MMMM Do YYYY');
 document.getElementById("currentDay").textContent = currentDate;
+
+
+$(".saveBtn").click(function (event) {
+    event.preventDefault();
+    var value = $(this).siblings(".time-block").val();
+    var time = $(this).parent().attr("id");
+    console.log($(this).parent().attr("id"))
+    localStorage.setItem("hour-"+time, value);
+  });
+
+// add ID to text areas
+// select text areas with jquery
+// target value with val method
+// assign localstorage.getitem to that value.
+nineHourBlock.val(localStorage.getItem("hour-nineAm"));
+tenHourBlock.val(localStorage.getItem("hour-tenAm"));
+elevenHourBlock.val(localStorage.getItem("hour-tenAm"));
+twelveHourBlock.val(localStorage.getItem("hour-twelvePm"));
+oneHourBlock.val(localStorage.getItem("hour-onePm"));
+twoHourBlock.val(localStorage.getItem("hour-twoPm"));
+threeHourBlock.val(localStorage.getItem("hour-threePm"));
+fourHourBlock.val(localStorage.getItem("hour-fourPm"));
+fiveHourBlock.val(localStorage.getItem("hour-fivePm"));
+
 
